097c4994a52bdf12da0496c3d4ad039f136c6bdc
ykiakao <ykiakao@gmail.com>
2026-05-22 22:40:32 -0500

    Disable runtime font fetching on web

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 
 void main() {
+  GoogleFonts.config.allowRuntimeFetching = false;
   runApp(const ProviderScope(child: UniConnectApp()));
 }
 
